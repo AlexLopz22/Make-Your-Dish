9c96eeafa8267a03d2a7e4f18178ae24edd20db9
AlexLopz22 <alex05brx@gmail.com>
2025-06-04 13:40:33 +0200

    Responsive v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,28 +5,37 @@ import FormularioContacto from './FormularioContacto';
 function Footer() {
     return (
         <>
-            <div className='w-full bg-[var(--color-principal)] text-[var(--color-blanco)] p-8 text-center flex flex-col items-center'>
-                <div className='flex w-[60%] justify-around items-center'>
-                    <img src="/logos/logo-blanco.webp" alt="Logo MYD Blanco" className="mb-5 h-32" />
-                    <div className='mb-5 flex items-center h-[300px]'>
-                        <div className='text-left'>
-                            <div><a href="#">- Cookies</a></div>
-                            <div><a href="#">- Términos y condiciones</a></div>
-                            <div><a href="#">- ¿Quiénes somos?</a></div>
+            <div className="w-full bg-[var(--color-principal)] text-[var(--color-blanco)] p-8 text-center flex flex-col items-center">
+                <div className="flex flex-col md:flex-row w-full md:w-[60%] justify-around items-center">
+
+                    {/* Logo */}
+                    <img
+                        src="/logos/logo-blanco.webp"
+                        alt="Logo MYD Blanco"
+                        className="mb-5 md:mb-0 h-20 md:h-32"
+                    />
+
+                    {/* Contenido links y formulario */}
+                    <div className="flex flex-col md:flex-row items-center md:items-start">
+
+                        {/* Links */}
+                        <div className="text-left mb-5 md:mb-0 md:mr-5">
+                            <div><a href="#" className="hover:underline">- Cookies</a></div>
+                            <div><a href="#" className="hover:underline">- Términos y condiciones</a></div>
+                            <div><a href="#" className="hover:underline">- ¿Quiénes somos?</a></div>
                         </div>
 
-                        <div className='border-l border-[var(--color-blanco)] mx-5 h-full'></div>
+                        {/* Separador solo en md+ */}
+                        <div className="hidden md:block border-l border-[var(--color-blanco)] mx-5 h-40"></div>
 
-                        <div className='text-left h-60'>
-                            
-                            <FormularioContacto/>
+                        {/* Formulario */}
+                        <div className="text-left w-full md:w-auto md:h-60">
+                            <FormularioContacto />
                         </div>
                     </div>
                 </div>
 
-
-
-                <div className='text-xs'>Creado por Alejandro López Olmedo</div>
+                <div className="text-xs mt-8">Creado por Alejandro López Olmedo</div>
             </div>
         </>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,9 @@ function Header() {
     const { usuario, logout } = useAuth();
     const [menuAbierto, setMenuAbierto] = useState(false);
     const menuRef = useRef();
+    const [menuMovilAbierto, setMenuMovilAbierto] = useState(false);
+    const toggleMenuMovil = () => setMenuMovilAbierto(!menuMovilAbierto);
+
 
     const toggleMenu = () => {
         setMenuAbierto(prev => !prev);
@@ -36,54 +39,96 @@ function Header() {
     }, []);
 
     return (
-        <div className='w-full h-28 flex justify-between items-center py-3.5 text-2xl px-7'>
-            <Link className='h-full' to="/">
-                <img className='h-full' src="/logos/logo.webp" alt="Logo MYD" />
+        <div className="w-full h-28 flex justify-between items-center py-3.5 text-2xl px-7 relative">
+
+            {/* Logo */}
+            <Link className="h-full" to="/">
+                <img className="h-full" src="/logos/logo.webp" alt="Logo MYD" />
             </Link>
 
-            <div className='text-center w-1/2 flex justify-between'>
-                <Link className='link' to="/recetas">Recetas</Link>
-                <Link className='link' to="/plan">Plan Semanal</Link>
-                <Link className='link' to={'/foro'}>Foro</Link>
+            {/* Links - versión escritorio */}
+            <div className="hidden md:flex text-center w-1/2 justify-between">
+                <Link className="link" to="/recetas">Recetas</Link>
+                <Link className="link" to="/plan">Plan Semanal</Link>
+                <Link className="link" to="/foro">Foro</Link>
             </div>
 
-            {usuario ? (
-                <div className="relative" ref={menuRef}>
-                    <div onClick={toggleMenu} className="flex items-center cursor-pointer">
-                        <img src="/iconos/usuario-icono.webp" alt="Usuario icono" className="mr-2" />
-                        <div>{usuario.nombre}</div>
+            {/* Botón de usuario o login */}
+            <div className="hidden md:block">
+                {usuario ? (
+                    <div className="relative" ref={menuRef}>
+                        <div onClick={toggleMenu} className="flex items-center cursor-pointer">
+                            <img src="/iconos/usuario-icono.webp" alt="Usuario icono" className="mr-2" />
+                            <div>{usuario.nombre}</div>
+                        </div>
+                        <AnimatePresence>
+                            {menuAbierto && (
+                                <motion.div
+                                    className="absolute right-0 mt-2 w-48 bg-[var(--color-blanco)] border rounded shadow-lg z-50 text-base"
+                                    initial={{ opacity: 0, scale: 0.95 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit={{ opacity: 0, scale: 0.95 }}
+                                    transition={{ duration: 0.2 }}
+                                >
+                                    <Link to="/usuario" className="block px-4 py-2 hover:bg-gray-100">Perfil</Link>
+                                    <div onClick={handleLogout} className="block px-4 py-2 hover:bg-gray-100">Logout</div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </div>
+                ) : (
+                    <Link className="bg-[var(--color-principal)] text-[var(--color-blanco)] px-3 py-1 boton-acceder" to="/login">Acceder</Link>
+                )}
+            </div>
 
-                    <AnimatePresence>
-                        {menuAbierto && (
-                            <motion.div
-                                className="absolute right-0 mt-2 w-48 bg-[var(--color-blanco)] border rounded shadow-lg z-50 text-base"
-                                initial={{ opacity: 0, scale: 0.95 }}
-                                animate={{ opacity: 1, scale: 1 }}
-                                exit={{ opacity: 0, scale: 0.95 }}
-                                transition={{ duration: 0.2 }}
-                            >
-                                <Link to="/usuario" className="block px-4 py-2 hover:bg-gray-100">
-                                    Perfil
-                                </Link>
-                                <div onClick={handleLogout} className="block px-4 py-2 hover:bg-gray-100">
+            {/* Botón menú hamburguesa - solo móvil */}
+            <button className="block md:hidden relative w-8 h-8" onClick={toggleMenuMovil}>
+                {/* Ícono de menú */}
+                <img
+                    src="/iconos/menu-icono.webp"
+                    alt="Abrir menú"
+                    className={`absolute inset-0 w-full h-full object-contain transition-opacity duration-300 ease-in-out ${menuMovilAbierto ? 'opacity-0' : 'opacity-100'
+                        }`}
+                />
+
+                {/* Ícono de cerrar */}
+                <img
+                    src="/iconos/cerrar-icono.webp"
+                    alt="Cerrar menú"
+                    className={`absolute inset-0 w-full h-full object-contain transition-opacity duration-300 ease-in-out ${menuMovilAbierto ? 'opacity-100' : 'opacity-0'
+                        }`}
+                />
+            </button>
+
+
+            {/* Menú móvil desplegable */}
+            <AnimatePresence>
+                {menuMovilAbierto && (
+                    <motion.div
+                        className="absolute top-full left-0 w-full bg-[var(--color-blanco)] shadow-md flex flex-col items-center py-4 z-40 text-xl overflow-hidden"
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: 'auto', opacity: 1 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        <Link className="py-2 w-full text-center border-b border-[var(--color-principal)] link" to="/recetas" onClick={toggleMenuMovil}>Recetas</Link>
+                        <Link className="py-2 w-full text-center border-b border-[var(--color-principal)] link" to="/plan" onClick={toggleMenuMovil}>Plan Semanal</Link>
+                        <Link className="py-2 w-full text-center border-b border-[var(--color-principal)] link" to="/foro" onClick={toggleMenuMovil}>Foro</Link>
+
+                        {usuario ? (
+                            <>
+                                <Link className="py-2 w-full text-center border-b border-[var(--color-principal)] link" to="/usuario" onClick={toggleMenuMovil}>Perfil</Link>
+                                <div className="py-2 w-full text-center border-b border-[var(--color-principal)] cursor-pointer link" onClick={() => { toggleMenuMovil(); handleLogout(); }}>
                                     Logout
                                 </div>
-                            </motion.div>
+                            </>
+                        ) : (
+                            <Link className="py-2 w-full text-center border-b border-gray-300" to="/login" onClick={toggleMenuMovil}>Acceder</Link>
                         )}
-                    </AnimatePresence>
+                    </motion.div>
+                )}
+            </AnimatePresence>
 
-                </div>
-            ) : (
-                <div>
-                    <Link
-                        className='bg-[var(--color-principal)] text-[var(--color-blanco)] px-3 py-1 boton-acceder'
-                        to={'/login'}
-                    >
-                        Acceder
-                    </Link>
-                </div>
-            )}
         </div>
     );
 }
